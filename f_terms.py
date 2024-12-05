96b1f3d9972f6df109b56048c7f4bf4a3d256e8c
kulsylg42024 <kulsylg@gmail.com>
2024-12-05 22:54:30 +0600

    cc

---
+++ f_terms.py
@@ -0,0 +1,11 @@
+def f_want_term(number1, possion1, number2, possition2, wt, f_n):
+    number1=print(int(input(" write the number :")))
+    possion1= print(int(input(" write the number possition:")))
+    number2=print(int(input(" write the number :")))
+    possion2= print(int(input(" write the number possition:")))
+    wt= print(int(input(" the number of terms want in the series:")))
+    f_n=number1*possion1+number2*possition2
+    
+    return f_n
+value=f_want_term(0,1,1,2, 3, f_n) 
+print("value") 
--- main_exam.py
+++ main_exam.py
@@ -1,1 +1,30 @@
 print("exam")
+
+import f_terms
+
+my_num = ()
+
+while True:
+    print("Welcome to  My F_Number App")
+    print("======================")
+    print("For Generate the Fibonacci series up to a specific number of terms Enter : 1")
+    print("For Generate the Fibonacci series up to a specific number of maximum value : 2")
+    print("For Exit Enter : 3") 
+    print("======================")
+
+    choice= input ("Enter Your Choice (1 to 3)  : ")
+
+    if choice=="1":
+        print("======================")
+        f_terms.f_want_term(my_num )
+        print("======================")
+    elif choice=="2":
+        
+        print("======================")
+    elif choice=="3":
+        print("======================")
+        print("Thanks for Using The App")
+        print("======================")
+        break
+    else:
+        print("Invalid Choice: Please Try Again")
